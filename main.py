7d9d0324aa878eb601bef103a3e4dd908da0f175
gertberd <maildefaultname@gmail.com>
2020-01-26 22:33:24 +0300

    Fix year, automate it

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from datetime import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+env = Environment(
+    loader=FileSystemLoader("."), autoescape=select_autoescape(["html", "xml"])
+)
+template = env.get_template("template.html")
+since_year = 1920
+current_year = datetime.now().year
+years = current_year - since_year
+rendered_page = template.render(years=years)
+with open("index.html", "w", encoding="utf8") as file:
+    file.write(rendered_page)
+server = HTTPServer(("0.0.0.0", 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
